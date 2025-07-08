3f58185ac1282f0443ddef4bdb5f858a23e82519
Avita Mumtahana <mumtavita@gmail.com>
2025-07-09 00:31:34 +0700

    edit username db

--- config/Database.php
+++ config/Database.php
@@ -2,8 +2,8 @@
 
 class Database {
     private $host = "localhost";
-    private $db_name = "projec15_ctrack_db";
-    private $username = "root";
+    private $db_name = "ctrack_db";
+    private $username = "projec15_ctrack";
     private $password = "@kaesquare123";
     public $conn;
 
